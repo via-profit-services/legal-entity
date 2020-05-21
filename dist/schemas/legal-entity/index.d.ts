05ecf9b31fe9a546c60244666bbae49bf8c29fb6
Vasily Novosad <delhsmail@gmail.com>
2020-05-21 11:02:23 +0500

    Build

--- dist/schemas/legal-entity/index.d.ts
+++ dist/schemas/legal-entity/index.d.ts
@@ -1,5 +1,4 @@
-import permissions from './permissions';
 import resolvers from './resolvers';
 import * as typeDefs from './schema.graphql';
 import service from './service';
-export { typeDefs, resolvers, permissions, service, };
+export { typeDefs, resolvers, service, };
