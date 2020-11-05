d76df7cd617e3ad1824338926382e52b72f0c084
Vasily Novosad <delhsmail@gmail.com>
2020-11-05 16:04:11 +0500

    Update deps

--- dist/schemas/legal-entity/resolvers/LegalEntitiesQuery.d.ts
+++
@@ -1,4 +0,0 @@
-import { IObjectTypeResolver } from '@via-profit-services/core';
-import { Context } from '../types';
-export declare const queryResolver: IObjectTypeResolver<any, Context>;
-export default queryResolver;
--- dist/schemas/legal-entity/resolvers/LegalEntityPayments.d.ts
+++
@@ -1,7 +0,0 @@
-import { IObjectTypeResolver } from '@via-profit-services/core';
-import { Context } from '../types';
-interface IParent {
-    id: string;
-}
-export declare const legalEntityPaymentResolver: IObjectTypeResolver<IParent, Context, any>;
-export default legalEntityPaymentResolver;
