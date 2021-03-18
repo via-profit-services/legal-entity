58ce8f3ff313f1284e63a6687034f1ff37ef479a
Vasily Novosad <delhsmail@gmail.com>
2021-03-18 14:53:38 +0500

    Migrations and Seeds

---
+++ .knex/seeds/geography-fill.js
@@ -0,0 +1,110 @@
+module.exports =
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 61975:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.seed = void 0;
+const ru = __importStar(__webpack_require__(1691));
+const list = [ru];
+function seed(knex) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return list.reduce((prev, { countries, states, cities }) => __awaiter(this, void 0, void 0, function* () {
+            yield prev;
+            // insert countries
+            yield knex.raw(`
+      ${knex('geographyCountries').insert(countries).toQuery()}
+      on conflict ("id") do update set
+      ${Object.keys(countries[0]).map((field) => `"${field}" = excluded."${field}"`).join(',')}
+    `);
+            // insert states
+            yield knex.raw(`
+      ${knex('geographyStates').insert(states).toQuery()}
+      on conflict ("id") do update set
+      ${Object.keys(states[0]).map((field) => `"${field}" = excluded."${field}"`).join(', ')}
+    `);
+            // insert cities
+            yield knex.raw(`
+      ${knex('geographyCities').insert(cities).toQuery()}
+      on conflict ("id") do update set
+      ${Object.keys(cities[0]).map((field) => `"${field}" = excluded."${field}"`).join(', ')}
+    `);
+        }), Promise.resolve());
+    });
+}
+exports.seed = seed;
+
+
+/***/ }),
+
+/***/ 1691:
+/***/ ((module) => {
+
+module.exports = require("@via-profit-services/geography/dist/countries/RU");;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	// module exports must be returned from runtime so entry inlining is disabled
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(61975);
+/******/ })()
+;
